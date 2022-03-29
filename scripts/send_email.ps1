208a51d125d5e9b7e0b954140893e332dd577846
Elián <elian.fabian.el.belaizi@gmail.com>
2022-03-29 17:15:26 +0200

    Create send_email.ps1

---
+++ scripts/send_email.ps1
@@ -0,0 +1,23 @@
+﻿$EmailFrom = “elian.fabian.el.belaizi@gmail.com”
+# https://myaccount.google.com/lesssecureapps
+# https://www.youtube.com/watch?v=cJnNv_rDTe4
+$EmailTo = “someone@gmail.com”
+
+$Subject = “Test”
+
+$Body = “Correo enviado desde PowerShell”
+
+$SMTPServer = “smtp.gmail.com”
+
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+
+$SMTPClient.EnableSsl = $true
+$Creds = (Get-Credential -Credential $EmailFrom)
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($Creds.UserName, $Creds.Password);
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+
+
+$scriptBlock =
+{
+	Write-Host "hola"
+}
