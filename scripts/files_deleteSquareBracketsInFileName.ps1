84e1721526a1b39f3c2d355483e343768b7a8e15
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-08 10:27:28 +0200

    Moved files to desktop-utils repository

--- scripts/files_deleteSquareBracketsInFileName.ps1
+++
@@ -1,7 +0,0 @@
-# files_deleteSquareBracketsInFileName.ps1
-
-# Deletes all the square brackets from all the file names
-
-# This is suppose to use when renaming several folders at one with square brackets (which can give you erros)
-
-(Get-ChildItem -File -Recurse) | Rename-Item -NewName { $_.Name -replace "[\[\]]" }
--- scripts/files_removeDirectoriesByNameRecursively.ps1
+++
@@ -1,5 +0,0 @@
-# files_removeDirectoriesByNameRecursively.ps1
-
-# https://stackoverflow.com/questions/3648142/how-can-i-recursively-delete-folder-with-a-specific-name-with-powershell
-
-Get-Childitem -Include "build" -Recurse -force | Remove-Item -Force -Recurse
