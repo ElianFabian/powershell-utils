d119d67c185e59d09c3a46ed6aa4ca7571d9e94a
Elián <elian.fabian.el.belaizi@gmail.com>
2022-07-09 20:36:35 +0200

    Refactor, fix, improve and rename Invoke-DownloadFilesFromWeb in WebRequestModule.psm1

--- Modules/WebRequestModule/WebRequestModule.psm1
+++ Modules/WebRequestModule/WebRequestModule.psm1
@@ -11,7 +11,7 @@
     Gets all the file and folder links from a web page (like a VPS).
 
     .DESCRIPTION
-    This method is supposed to be used to download files and folders in Invoke-Download
+    This method is supposed to be used to download files and folders in Invoke-DownloadFilesFromWeb
     and to do so it's useful to know what is a folder and what is a file when we introduce a url
     it's going to be a folder which will contain more folders and files.
 
@@ -21,7 +21,7 @@
 function Invoke-FileLinksFromUri([string] $Uri, [switch] $Verbose)
 {
     # If the Uri doesn't ends with a slash then we add it because it's supposed to be a folder,
-    # ending with a slash it's what we use to differentiate between files and folders.
+    # ending with a backslash it's what we use to differentiate between files and folders.
     if (-Not $Uri.EndsWith("/"))
     {
         $Uri += "/"
@@ -33,9 +33,9 @@ function Invoke-FileLinksFromUri([string] $Uri, [switch] $Verbose)
 
     $linkCount = $allLinksFromWebResponse.Links.Count
 
-    0..($linkCount - 1) | ForEach-Object {
-
-        $currentLink = $allLinksFromWebResponse.Links.Item($_)
+    for ($i = 0; $i -lt $linkCount; $i++)
+    {
+        $currentLink = $allLinksFromWebResponse.Links.Item($i)
 
         if ($currentLink.innerHTML -eq $currentLink.href) # The links we want satisfy this condition
         {
@@ -43,82 +43,100 @@ function Invoke-FileLinksFromUri([string] $Uri, [switch] $Verbose)
         }
     }
 
-	return $linkList
+	return $linkList.AsReadOnly()
 }
 
-# This is the prive version of Invoke-Download, we have to define the other function in other to
+# This is the prive version of Invoke-DownloadFilesFromWeb, we have to define the other function in other to
 # make the files be contained in the folder given in the Uri.
-function Invoke-Download-WithoutContainingFolder([string] $Uri, [string] $Destination = ".\", [switch] $Recurse , [switch] $Verbose)
-{
-    $links = Invoke-FileLinksFromUri -Uri $Uri -Verbose:$Verbose
+function Invoke-DownloadFilesFromWeb_WithoutContainingFolder
+(
+    [string] $Uri,
+    [string] $OutFolder = "./",
+    [switch] $Recurse ,
+    [switch] $Verbose
+) {
+    $linkList = Invoke-FileLinksFromUri -Uri $Uri -Verbose:$Verbose
 
-    foreach($link in $links)
+    foreach($link in $linkList)
     {
-        $splittedLink = $link.Split("/")
-
         $isFolder = $link.EndsWith("/")
 
         if ($isFolder)
         {
-            $folderName = $splittedLink[$splittedLink.Length - 2]
+            $folderName = $link | Split-Path -Leaf
 
-            New-Item -Path "$Destination\$folderName\" -ItemType Directory
+            $null = New-Item -Path "$OutFolder/$folderName" -ItemType Directory
 
             if ($Recurse)
             {
                 try
                 {
-                    Invoke-Download-WithoutContainingFolder -Uri "$Uri$folderName/" -Destination "$Destination\$folderName\" -Recurse:$Recurse -Verbose:$Verbose
+                    Invoke-DownloadFilesFromWeb_WithoutContainingFolder -Uri $link -OutFolder "$OutFolder/$folderName/" -Recurse:$Recurse -Verbose:$Verbose
+                }
+                catch [System.Net.WebException]
+                {
+                    Write-Warning "$_`nRelated link: $link"
                 }
                 catch # This will be thrown if a file has no extension
                 {
+                    Write-Warning "$_`nFile has no extension: $link"
+
+                    Remove-Item -Path "$OutFolder/$folderName/" -Recurse
+
                     $fileName = $folderName
 
-                    Write-Warning "The file '$Uri$fileName' has no extension."
-
-                    Remove-Item -Path "$Destination\$fileName\"
-
-                    Invoke-WebRequest -Uri $link -OutFile "$Destination\$fileName" -Verbose:$Verbose
-
-                    continue
+                    Invoke-WebRequest -Uri $link -OutFile "$OutFolder/$fileName" -Verbose:$Verbose
                 }
             }
         }
         else
         {
-            $fileName = $splittedLink[$splittedLink.Length - 1]
+            $fileName = Split-Path $link -Leaf
 
-            Invoke-WebRequest -Uri $link -OutFile "$Destination\$fileName" -Verbose:$Verbose
+            Invoke-WebRequest -Uri $link -OutFile "$OutFolder/$fileName" -Verbose:$Verbose
         }
     }
 }
 
 <#
     .SYNOPSIS
-    Gets content from a web page on the Internet (like a VPS).
+    Gets content from a web page on the Internet of a folder explore type.
 
     .DESCRIPTION
     Downloads all the files and folders from a url inside the folder they are contained in a web page (for example a VPS).
-
-    .PARAMETER Uri
-    The URI of the web page.
-
-    .PARAMETER Destination
-    The destination folder where the files will be downloaded, by default it's the current folder.
-
-    .PARAMETER Recurse
-    If present it downloads all the files from every single folder recursively.
 #>
-function Invoke-Download([string] $Uri, [string] $Destination = ".\", [switch] $Recurse, [switch] $Verbose)
-{
-    $splittedUri    = $Uri.Split("/")
-    $rootFolderName = $splittedUri[$splittedUri.Length - 2]
+function Invoke-DownloadFilesFromWeb
+(
+    [string] $Uri,
+    [string] $OutFolder = "./",
+    [switch] $Recurse,
+    [switch] $Verbose,
+    [switch] $ForceFastDownload
+){
+    $rootFolderName = $Uri | Split-Path -Leaf
+    
+    $OutFolder = Join-Path -Path $OutFolder -ChildPath $rootFolderName
 
-    $Destination += "$rootFolderName\"
+    $null = New-Item -Path $OutFolder -ItemType Directory
 
-    New-Item -Path $Destination -ItemType Directory
+    $previousProgressPreference = $ProgressPreference
 
-    Invoke-Download-WithoutContainingFolder -Uri $Uri -Destination $Destination -Recurse:$Recurse -Verbose:$Verbose
+    if ($ForceFastDownload)
+    {
+        $ProgressPreference = 'SilentlyContinue'
+
+        Write-Verbose "Disable Progressbar because of ForceFastDownload" -Verbose
+
+        if ($Verbose)
+        {
+            Write-Verbose "Disable Verbose because of ForceFastDownload" -Verbose
+            $Verbose = $false
+        }
+    }
+
+    Invoke-DownloadFilesFromWeb_WithoutContainingFolder -Uri $Uri -OutFolder $OutFolder -Recurse:$Recurse -Verbose:$Verbose
+
+    $ProgressPreference = $previousProgressPreference
 }
 
 
@@ -139,5 +157,5 @@ function Invoke-FileContentExpression([string] $Uri)
 
 
 Export-ModuleMember -Function `
-    Invoke-Download,
+    Invoke-DownloadFilesFromWeb,
     Invoke-FileContentExpression
