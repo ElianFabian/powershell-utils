56d66d207501245e34bc63b7a9e4e046be871b44
Elián <elian.fabian.el.belaizi@gmail.com>
2022-09-04 02:47:09 +0200

    Create Unsetup.ps1

---
+++ Scripts/ModuleManagement/Unsetup.ps1
@@ -0,0 +1,10 @@
+# Setup.ps1
+
+
+
+# From repository: https://github.com/ElianFabian/powershell-utils
+
+
+
+& ./Remove-UsingModuleStatements-InProfile.ps1
+& ./Remove-Modules-InEnvPSModulePath.ps1
