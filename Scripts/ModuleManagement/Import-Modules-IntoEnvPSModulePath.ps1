df500fe8e69f6b3b2da72ea59c82aa27cbae68a3
Elián <elian.fabian.el.belaizi@gmail.com>
2022-09-04 02:34:47 +0200

    Update Import-Modules-IntoEnvPSModulePath.ps1

--- Scripts/ModuleManagement/Import-Modules-IntoEnvPSModulePath.ps1
+++ Scripts/ModuleManagement/Import-Modules-IntoEnvPSModulePath.ps1
@@ -11,7 +11,7 @@ $userModulePath = $env:PSModulePath.Split(";")[0]
 # In case the folder doesn't exist, create it (This is for Porweshell 7 support)
 New-Item $userModulePath -ItemType Directory -ErrorAction SilentlyContinue
 
-Copy-Item -Path ..\..\Modules\* -Destination $userModulePath -Recurse
+Copy-Item -Path ..\..\Modules\* -Destination $userModulePath -Recurse -Force
 
 
 # Invoke-Item -Path $userModulePath
