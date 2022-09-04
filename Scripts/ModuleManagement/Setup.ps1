97d14eba77f305c8edc17980874fb270ac143344
Elián <elian.fabian.el.belaizi@gmail.com>
2022-09-04 02:48:04 +0200

    Update Setup.ps1

--- Scripts/ModuleManagement/Setup.ps1
+++ Scripts/ModuleManagement/Setup.ps1
@@ -5,6 +5,7 @@
 # From repository: https://github.com/ElianFabian/powershell-utils
 
 
-& .\Autogenerate-Modules.ps1
-& .\Import-Modules-IntoEnvPSModulePath.ps1
-& .\Update-UsingModuleStatements-InProfile.ps1
+
+& ./Autogenerate-Modules.ps1
+& ./Import-Modules-IntoEnvPSModulePath.ps1
+& ./Update-UsingModuleStatements-InProfile.ps1
