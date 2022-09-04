ced907a7305992bcf4db0a267edf992511e4f0ce
Elián <elian.fabian.el.belaizi@gmail.com>
2022-09-04 19:55:01 +0200

    Renaming

---
+++ Scripts/Utils/InsertOrUpdate-ValueInString.ps1
@@ -0,0 +1,46 @@
+# InsertOrUpdate-ValueInString.ps1
+
+
+
+# From repository: https://github.com/ElianFabian/powershell-utils
+
+
+
+param(
+    [string] $ValueToInsertOrUpdate,
+    [string] $ValueRegex,
+    [string] $ValueKey,
+    [string] $SourceString,
+    [Scriptblock] $InsertValue = {
+        param($sourceString, $valueToInsert)
+
+        return $sourceString + $valueToInsert
+    },
+    [scriptblock] $UpdateValue = {
+        param($updatedText)
+
+        return $updatedText
+    }
+)
+
+
+
+$textMatches = ($SourceString | Select-String -Pattern $ValueRegex -AllMatches).Matches
+
+#region Add
+if ($null -eq $textMatches)
+{
+    return $InsertValue.Invoke($SourceString, $ValueToInsertOrUpdate)
+}
+#endregion
+#region Update
+else
+{
+    foreach ($match in $textMatches)
+    {
+        if (-not $match.Value.Contains($ValueKey)) { continue }
+
+        return $SourceString -replace $match.Value, $UpdateValue.Invoke($ValueToInsertOrUpdate)
+    }
+}
+#endregion
