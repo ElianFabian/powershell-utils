1b6e326bd2be3557c114e16f1b1588bcae084036
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-10-28 21:08:04 +0200

    Created Convert-ItemToItemWithRegex function

--- Modules/String.Manipulation/String.Manipulation.psm1
+++ Modules/String.Manipulation/String.Manipulation.psm1
@@ -39,3 +39,57 @@ function AddOrUpdate-ValueInString
         }
     }
 }
+
+<#
+    .SYNOPSIS
+    Given a string gets every item using a pattern to convert it into another string with items.  
+    .PARAMETER InputObject
+    A string of items that matches a certain pattern.  
+    .PARAMETER ItemPattern
+    The pattern to match each item of the given InputObject.  
+    .PARAMETER ItemSeparator
+    It's the string that separates each item, by default it's a new line.  
+    .PARAMETER OnCreateItem
+    An script block which $args contains all the groups defined in $ItemPattern and returns an item as string.  
+    .EXAMPLE
+    Convert-ItemToItemWithRegex `
+        -InputObject @"
+        <string name="name">Alice</string>
+        <string name="age">25</string>
+        "@ `
+        -ItemPattern '<string name="(.+)">(.+)<\/string>' `
+        -OnCreateItem { $name, $content = $args  
+            "$name = ""$content"""
+        }
+    output:
+        name = "Alice"
+        age = 25
+#>
+function Convert-ItemToItemWithRegex
+(
+    [string]      $InputObject,
+    [string]      $ItemPattern,
+    [string]      $ItemSeparator = "`n",
+    [scriptblock] $OnCreateItem
+) {
+    $resultSB = [System.Text.StringBuilder]::new()
+
+    $currentIndex = 0
+    $separator    = ''
+
+    $allMatches = $InputObject | Select-String -Pattern $ItemPattern -AllMatches | Select-Object -ExpandProperty Matches
+
+    foreach ($match in $allMatches)
+    {
+        $_first, $groups = $match.Groups | Select-Object -ExpandProperty Value
+
+        $newItem = & $OnCreateItem @groups
+
+        $resultSB.Append("$separator$newItem") > $null
+
+        $currentIndex++
+        $separator = $ItemSeparator
+    }
+
+    return $resultSB.ToString()
+}
