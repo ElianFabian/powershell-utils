0535882051118a612c3d40ad1191049ee2737da1
Elián Fabián <elian.fabian.2001@gmail.com>
2024-06-25 21:01:55 +0200

    Update XorWowRandom.ps1

--- Utils/XorWowRandom.ps1
+++ Utils/XorWowRandom.ps1
@@ -13,7 +13,9 @@ class XorWowRandom {
         $this.z = 0
         $this.w = 0
         $this.v = -bnot $seed1
-        $this.addend = ($seed1 -shl 10) -bxor [int]::CreateTruncating([uint]::CreateTruncating($seed2) -shr 4)
+        $this.addend = ($seed1 -shl 10) -bxor [int]::CreateTruncating(
+            [uint]::CreateTruncating($seed2) -shr 4
+        )
 
         if (($this.x -bor $this.y -bor $this.z -bor $this.w -bor $this.v) -eq 0) {
             throw "Initial state must have at least one non-zero element."
@@ -24,6 +26,22 @@ class XorWowRandom {
             $this.NextInt() > $null
         }
     }
+
+    static [XorWowRandom] FromSeed([int] $seed) {
+        return [XorWowRandom]::new($seed, $seed -shr 31)
+    }
+
+    static [XorWowRandom] FromSeed([long] $seed) {
+        return [XorWowRandom]::new([int]::CreateTruncating($seed), [int]::CreateTruncating($seed -shr 31))
+    }
+
+
+    [int] NextBits([int] $bitCount) {
+        return [int]::CreateTruncating(
+            [uint]::CreateTruncating($this.NextInt()) -shr (32 - $bitCount)
+        ) -band (-$bitCount -shr 31)
+    }
+
 
     [int] NextInt() {
         # Equivalent to the xorxow algorithm
@@ -41,14 +59,188 @@ class XorWowRandom {
         return $t + $this.addend
     }
 
-    [int] NextBits([int] $bitCount) {
-        return [int]::CreateTruncating(
-            [uint]::CreateTruncating($this.NextInt()) -shr (32 - $bitCount)
-        ) -band (-$bitCount -shr 31)
+    [int] NextInt([int] $until) {
+        return $this.NextInt(0, $until)
     }
-}
+    
+    [int] NextInt([int] $from, [int] $until) {
+        if ($until -le $from) {
+            throw "Random range is empty: [$from, $until)."
+        }
+
+        $n = $until - $from
+        if ($n -gt 0 -or $n -eq [int]::MinValue) {
+            if (($n -band - $n) -eq $n) {
+                $bitCount = [Math]::ILogB($n)
+                $rnd = $this.NextBits($bitCount)
+            }
+            else {
+                $value = 0
+                do {
+                    $bits = [int]::CreateTruncating(
+                        [uint]::CreateTruncating($this.NextInt()) -shr 1
+                    )
+                    $value = $bits % $n
+                } while ($bits - $value + ($n - 1) -lt 0)
+                $rnd = $value
+            }
+            return $from + $rnd
+        }
+        else {
+            while ($true) {
+                $rnd = $this.NextInt()
+                if ($rnd -ge $from -and $rnd -lt $until) {
+                    return $rnd
+                }
+            }
+        }
+
+        throw "This should never happen"
+    }
+
+    [long] NextLong() {
+        return (([long] $this.NextInt()) -shl 32) + $this.NextInt()
+    }
+
+    [long] NextLong([long] $until) {
+        return $this.NextLong(0, $until)
+    }
+
+    [long] NextLong([long] $from, [long] $until) {
+        if ($until -le $from) {
+            throw "Random range is empty: [$from, $until)."
+        }
+    
+        $n = $until - $from
+        if ($n -gt 0) {
+            $rnd = 0L
+            if (($n -band - $n) -eq $n) {
+                $nLow = [int]::CreateTruncating($n)
+                $nHigh = [int]::CreateTruncating(
+                    [uint]::CreateTruncating($n) -shr 32
+                )
+
+                if ($nLow -ne 0) {
+                    $bitCount = [Math]::ILogB($nLow)
+                    $rnd = [long] $this.NextBits($bitCount) -band 0xFFFFFFFFL
+                }
+                elseif ($nHigh -eq 1) {
+                    $rnd = [long] $this.NextInt() -band 0xFFFFFFFFL
+                }
+                else {
+                    $bitCount = [Math]::ILogB($nHigh)
+                    $rnd = ([long] $this.NextBits($bitCount) -shl 32) + ([long] $this.NextInt() -band 0xFFFFFFFFL)
+                }
+            }
+            else {
+                $value = 0L
+                do {
+                    $bits = [long]::CreateTruncating(
+                        [ulong]::CreateTruncating($this.NextLong()) -shr 1
+                    )
+                    $value = $bits % $n
+                } while ($bits - $value + ($n - 1) -lt 0)
+
+                $rnd = $value
+            }
+            return $from + $rnd
+        }
+        else {
+            while ($true) {
+                $rnd = $this.NextLong()
+                if ($rnd -ge $from -and $rnd -lt $until) {
+                    return $rnd
+                }
+            }
+        }
+    
+        throw "This should never happen"
+    }
+
+    [bool] NextBoolean() {
+        return $this.NextBits(1) -ne 0
+    }
+
+    [double] NextDouble() {
+        $hi26 = [long] $this.NextBits(26)
+        $low27 = $this.NextBits(27)
+
+        return ((($hi26 -shl 27L) + $low27) / [double] (1L -shl 53))
+    }
+
+    [double] NextDouble([double] $until) {
+        return $this.NextDouble(0.0, $until)
+    }
+
+    [double] NextDouble([double] $from, [double] $until) {
+        if ($until -le $from) {
+            throw "Random range is empty: [$from, $until)."
+        }
+
+        $size = $until - $from
+        if ([double]::IsInfinity($size) -and [double]::IsFinite($from) -and [double]::IsFinite($until)) {
+            $r1 = $this.NextDouble() * ($until / 2 - $from / 2)
+            $r = $from + $r1 + $r1
+        }
+        else {
+            $r = $from + $this.NextDouble() * $size
+        }
+        if ($r -ge $until) {
+            return [math]::NextDown($until)
+        }
+        else {
+            return $r
+        }
+    }
+
+    [float] NextFloat() {
+        return $this.NextBits(24) / [float] (1 -shl 24)
+    }
 
 
-function New-XorWowRandom([int] $Seed) {
-    return [XorWowRandom]::new($Seed, $Seed -shr 31)
+    [sbyte[]] NextBytes([sbyte[]] $array, [int] $fromIndex = 0, [int] $toIndex = $array.Length) {
+        if ($fromIndex -lt 0 -or $toIndex -gt $array.Length) {
+            throw "fromIndex ($fromIndex) or toIndex ($toIndex) are out of range: 0..$($array.Length)."
+        }
+        if ($fromIndex -gt $toIndex) {
+            throw "fromIndex ($fromIndex) must be not greater than toIndex ($toIndex)."
+        }
+    
+        $steps = [math]::Floor(($toIndex - $fromIndex) / 4)
+    
+        $position = $fromIndex
+        for ($i = 0; $i -lt $steps; $i++) {
+            $value = $this.NextInt()
+            $array[$position] = [sbyte]::CreateTruncating($value)
+            $array[$position + 1] = [sbyte]::CreateTruncating(
+                [uint]::CreateTruncating($value) -shr 8
+            )
+            $array[$position + 2] = [sbyte]::CreateTruncating(
+                [uint]::CreateTruncating($value) -shr 16
+            )
+            $array[$position + 3] = [sbyte]::CreateTruncating(
+                [uint]::CreateTruncating($value) -shr 24
+            )
+            $position += 4
+        }
+    
+        $remainder = $toIndex - $position
+        $vr = $this.NextBits($remainder * 8)
+        for ($i = 0; $i -lt $remainder; $i++) {
+            $array[$position + $i] = [sbyte]::CreateTruncating(
+                [uint]::CreateTruncating($vr) -shr ($i * 8)
+            )
+        }
+    
+        return $array
+    }
+
+    [sbyte[]] NextBytes([sbyte[]] $array) {
+        return $this.NextBytes($array, 0, $array.Length)
+    }
+    
+    [sbyte[]] NextBytes([int] $size) {
+        $array = New-Object sbyte[]($size)
+        return $this.NextBytes($array)
+    }
 }
